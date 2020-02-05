d7014df6990d730052a5ac99610e78ffe1f6655a
JhonathanAlejandro01 <joaleanco@hotmail.com>
2020-02-05 21:22:31 +0000

    4 second

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
- *  prints exactly
+ * main - prints exactly
  *
  * Return: Always 0 (Success)
  */
-int main ()
+int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
