594ef4eef4c8cae48c84c7d8a467dbf503da68f6
JhonathanAlejandro01 <joaleanco@hotmail.com>
2020-02-08 03:27:02 +0000

    8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+
+#include <stdio.h>
+
+/**
+ * main - base 16
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char a;
+
+	char b;
+
+	for (a = 48; a < 58; a++)
+	{
+		putchar(a);
+	}
+	for (b = 97; b < 103; b++)
+	{
+		putchar(b);
+	}
+	putchar(10);
+	return (0);
+}
