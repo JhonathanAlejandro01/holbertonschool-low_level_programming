169fa17854a6402394e12248dead3cabfe1c1e4b
JhonathanAlejandro01 <joaleanco@hotmail.com>
2020-02-08 03:06:07 +0000

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 48; numbers <= 57; numbers++)
+	{
+		putchar(numbers);
+	}
+	putchar(10);
+	return (0);
+}
