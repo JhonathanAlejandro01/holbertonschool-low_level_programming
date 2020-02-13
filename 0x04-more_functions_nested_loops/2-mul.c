42f94fcdc653d1a1f929012bcdfa8a080bb7a08c
JhonathanAlejandro01 <joaleanco@hotmail.com>
2020-02-13 12:36:50 -0500

    second commit 1 and 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "holberton.h"
+/**
+ * mul - is for multiply.
+ *@a: it is variable
+ *@b: it is variable
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -8,5 +8,7 @@ int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
 void print_remaining_days(int month, int day, int year);
 int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
 
 #endif /* HOLBERTON_H */
