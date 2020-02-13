ab369118b349c522267f8ce945bea1ef73377643
JhonathanAlejandro01 <joaleanco@hotmail.com>
2020-02-13 16:31:28 -0500

    7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+/**
+ * print_diagonal - draws diagonal line.
+ * @n: is parameter
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	int j;
+
+	int h;
+
+	if (n > 0){
+	for (j = 0; j < n; j++)
+	{
+		for(h = 0; h < j; h++)
+		{
+			_putchar(' ');
+		}
+
+		_putchar(92);
+		_putchar('\n');
+	}
+	}
+	else{
+	_putchar ('\n');
+	}
+}
