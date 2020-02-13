bd17d62e3a6f7d9c43b757d6aa70c9c276b56ad8
JhonathanAlejandro01 <joaleanco@hotmail.com>
2020-02-13 15:26:49 -0500

    6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ * print_line - prints digits.
+ * @n: is parameter
+ * Return: Always 0.
+ */
+void print_line(int n)
+{int j;
+	for (j = 0; j <= n; j++)
+	{
+		_putchar(95);
+	}
+	_putchar ('\n');
+}
