3332b7004f072a96ef290ec135d5153828acef16
JhonathanAlejandro01 <joaleanco@hotmail.com>
2020-02-18 19:55:43 -0500

    5 second

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * rev_string - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void rev_string(char *s)
+{
+	int x, q;
+	int y, a;
+
+	for (y = 0; s[y] != '\0'; y++)
+	{
+	a = y;
+	}
+	for (x = 0; x < y / 2; x++)
+	{
+		q = s[x];
+		s[x] = s[a];
+		s[a] = q;
+		a--;
+
+	}
+
+}
