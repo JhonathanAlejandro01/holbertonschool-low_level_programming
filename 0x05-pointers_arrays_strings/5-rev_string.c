0562a362c57e86e2544cfa0f935921c3171f8ab5
JhonathanAlejandro01 <joaleanco@hotmail.com>
2020-02-18 20:01:53 -0500

    5 tired 3

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -2,7 +2,7 @@
 
 /**
  * rev_string - check the code for Holberton School students.
- *
+ * @s: is parameter
  * Return: Always 0.
  */
 void rev_string(char *s)
@@ -12,7 +12,7 @@ void rev_string(char *s)
 
 	for (y = 0; s[y] != '\0'; y++)
 	{
-	a = y;
+		a = y;
 	}
 	for (x = 0; x < y / 2; x++)
 	{
