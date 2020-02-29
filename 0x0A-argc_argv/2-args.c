9a797eabf027f818d8e304906758968d5c3cb152
JhonathanAlejandro01 <joaleanco@hotmail.com>
2020-02-28 19:29:20 -0500

    2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - is fuction.
+ *@argc: count variable
+ *@argv: return of values of program.
+ *Return: devolvel 0.
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	for(i = 0; i <= argc -1; i++)
+	{
+		
+	}
+}
