31568cf7ac31d18713bbc34d1edbb4f34cbe53a4
JhonathanAlejandro01 <joaleanco@hotmail.com>
2020-03-03 18:53:06 -0500

    2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,1 +1,35 @@
 #include "holberton.h"
+#include <stdlib.h>
+/**
+ * *str_concat - check the code for Holberton School students.
+ *@s1: is pointer (betty).
+ *@s2: is poniter (holberton).
+ * Return: Betty Holberton.
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+	char *p;
+
+	for (a = 0; s1[a] != '\0'; a++)
+	{}
+	for (b = 0; s2[b] != '\0'; b++)
+	{}
+	p = malloc((a + b) * sizeof(char));
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (c = 0; s1[c] != '\0'; c++)
+	{
+		p[c] = s1[c];
+	}
+	for (d = 0; s2[d] != '\0'; d++)
+	{
+		p[c + d] = s2[d];
+	}
+	return (p);
+}
