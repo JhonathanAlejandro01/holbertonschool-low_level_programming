54847da562c20b26c5fbc95d113bed6754e3cd5f
JhonathanAlejandro01 <joaleanco@hotmail.com>
2020-03-05 16:50:30 -0500

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,5 +11,48 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	return;
+	int a;
+	unsigned int b;
+	unsigned int c;
+	unsigned int d;
+	char *p;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	for (a = 0; s1[a] != '\0'; a++)
+	{
+	}
+	for (b = 0; s2[b] != '\0'; b++)
+	{
+	}
+	if (n >= b)
+	{
+		n = b;
+		p = malloc(b + n + 1);
+	}
+	else if (n < b)
+	{
+		p = malloc((a + b + 1) * sizeof(char));
+	}
+	if (p == NULL)
+	{
+		return (NULL);
+	}
+	for (c = 0; s1[c] != '\0'; c++)
+	{
+		p[c] = s1[c];
+	}
+	for (d = 0; s2[d] != '\0'; d++)
+	{
+		p[c++] = s2[d];
+	}
+	p[c++] = '\0';
+	return (p);
 }
