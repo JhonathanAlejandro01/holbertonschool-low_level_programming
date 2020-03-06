8593e7f3525962a9450deff38ecf73403c0f4373
JhonathanAlejandro01 <joaleanco@hotmail.com>
2020-03-05 23:16:15 -0500

    3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -12,5 +12,28 @@
  */
 int *array_range(int min, int max)
 {
+
+	int i;
+	int j;
+	int *a;
+ 
+	if (min > max)
+	{
+		return (NULL);
+	}
+ 
+	j = max - min + 1;
+	a = malloc(sizeof(int) * j);
+	if (a == NULL)
+	{
+		return (NULL);
+	}
+ 
+	for (i = 0; i < j; i++, min++)
+	{
+		a[i] = min;
+	}
+ 
+	return (a);
 	
 }
