51b438bf5a75ff44042482dba384704ff339cd75
JhonathanAlejandro01 <joaleanco@hotmail.com>
2020-03-09 20:11:25 -0500

    4 task

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ *new_dog - check the code for Holberton School students.
+ *@name: is name
+ *@age: is a age
+ *@owner: is a owner
+ * Return: Always 0.
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	struct dog *new_dog;
+
+	new_dog = malloc(sizeof(struct dog));
+
+	if (new_dog == NULL)
+	{
+		return (NULL);
+	}
+	new_dog->name = name;
+	new_dog->age = age;
+	new_dog->owner = owner;
+	return (new_dog);
+}
