06750196338e89c39119195f7cbe4a44ab60439d
JhonathanAlejandro01 <joaleanco@hotmail.com>
2020-03-11 18:44:29 -0500

    1

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,31 @@
+#include <stidio.h>
+/**
+ * op_add - operator add
+ *@a: is a integer number
+ *@b: is a integer number
+ * Return: total
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - operator add
+ *@a: is a integer number
+ *@b: is a integer number
+ * Return: total
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - operator add
+ *@a: is a integer number
+ *@b: is a integer number
+ * Return: total
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,6 @@
 #ifndef _FUNTIONS_POINTERS_H_
 #define _FUNTIONS_POINTERS_H_
 void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif
