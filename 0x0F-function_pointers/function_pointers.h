ed7824aba4b93a065740b72666fa38b6922ad6a6
JhonathanAlejandro01 <joaleanco@hotmail.com>
2020-03-11 19:58:17 -0500

    2

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,7 @@
 #ifndef _FUNTIONS_POINTERS_H_
 #define _FUNTIONS_POINTERS_H_
 void print_name(char *name, void (*f)(char *));
-void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif
