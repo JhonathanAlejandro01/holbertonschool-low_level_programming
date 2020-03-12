c456c5fb8bf4f24f992518a98712fbf77038c1d5
JhonathanAlejandro01 <joaleanco@hotmail.com>
2020-03-11 19:21:09 -0500

    test 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -29,3 +29,33 @@ int op_mul(int a, int b)
 {
 	return (a * b);
 }
+/**
+ * op_div - operator add
+ *@a: is a integer number
+ *@b: is a integer number
+ * Return: total
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf ("Error\n");
+		exit(100);
+	}
+	return (a / b);
+}
+/**
+ * op_mod - operator add
+ *@a: is a integer number
+ *@b: is a integer number
+ * Return: total
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf ("Error\n");
+		exit(100);
+	}
+	return (a % b);
+}
