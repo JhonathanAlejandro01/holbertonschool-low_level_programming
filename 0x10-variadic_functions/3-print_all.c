164105f2108db14801185e1b546ea6625e09e31d
DavidDlhz <vidda1959@gmail.com>
2020-03-14 16:50:28 -0500

    3

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,48 @@
+#include "variadic_functions.h"
+/**
+ * print_all -prints all character.
+ *@format: is a contain.
+ * Return: Always 0.
+ */
+void print_all(const char * const format, ...)
+{
+	int format_i = 0;
+	va_list valist;
+	char *my_p;
+	int flag_flound;
+
+	va_start(valist, format);
+	while (format[format_i] != '\0')
+	{
+		flag_flound = 1;
+		switch (format[format_i])
+		{
+		case 'c':
+			printf("%c", va_arg(valist, int));
+			break;
+		case 'i':
+			printf("%d", va_arg(valist, int));
+			break;
+		case 'f':
+			printf("%f", va_arg(valist, double));
+			break;
+		case 's':
+			my_p = va_arg(valist, char*);
+			if (my_p == NULL)
+			{
+				printf("(nil)");
+				break;
+			}
+			printf("%s", my_p);
+			break;
+		default:
+			flag_flound = 0;
+			break;
+		}
+		format_i++;
+		if (flag_flound == 1 && format[format_i] != '\0')
+			printf(", ");
+	}
+	va_end(valist);
+	printf("\n");
+}
