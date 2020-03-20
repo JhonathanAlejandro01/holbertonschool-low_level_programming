de1915ab2cb0e60d78789ee6b9c65888684523cf
JhonathanAlejandro01 <joaleanco@hotmail.com>
2020-03-19 22:26:29 -0500

    1

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -1,6 +1,17 @@
 #include "lists.h"
 /**
- *
- *
- *
+ * list_len - counter the elements of list.
+ *@h: element lista.
+ * Return: size_t is a integer unsigned.
  */
+size_t list_len(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+	return (count);
+}
