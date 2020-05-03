90aa8518b8acf82e8d7873d27a5145b738126d80
JhonathanAlejandro01 <joaleanco@hotmail.com>
2020-05-02 21:36:19 -0500

    2

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * add_dnodeint - add a new nodo the strat of list.
+ *@head: header.
+ *@n: elements of the list.
+ * Return: the adress the new elementos o NULL if break.
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new_nodo;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	new_nodo = malloc(sizeof(dlistint_t));
+	new_nodo->n = n;
+	new_nodo->next = NULL;
+	new_nodo->prev = NULL;
+	if (*head != NULL)
+	{
+		new_nodo->next = *head;
+		(*head)->prev = new_nodo;
+		*head = new_nodo;
+	}
+	else
+	{
+		*head = new_nodo;
+	}
+
+	return (new_nodo);
+}
