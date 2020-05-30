41cc6dee5569f8d3442d5d807182939b178ee735
JhonathanAlejandro01 <joaleanco@hotmail.com>
2020-05-29 21:31:00 -0500

    0

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - prints string in Reverse.
+ * @size: is the size of the array.
+ * Return: Hash table.
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht;
+
+	if (size < 1)
+	{
+		return (NULL);
+	}
+
+	ht = malloc(sizeof(hash_table_t));
+
+	if (ht == NULL)
+	{
+		return (NULL);
+	}
+
+	ht->size = size;
+
+	ht->array = malloc(sizeof(hash_node_t));
+
+	if (ht->array == NULL)
+	{
+		free(ht);
+		return (NULL);
+	}
+
+	return (ht);
+}
