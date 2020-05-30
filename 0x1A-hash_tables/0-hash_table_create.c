1d08e88a7e48d8dc446683d9f4195fc3ac3dc9f1
JhonathanAlejandro01 <joaleanco@hotmail.com>
2020-05-29 22:48:24 -0500

    fix betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,36 +1,29 @@
 #include "hash_tables.h"
 /**
- * hash_table_create - prints any string in Reverse.
- * @size: is the size of the array
- * Return: Hash table.
+ *hash_table_create - create a hash table
+ *@size: size of array
+ *Return: a new hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *ht;
+	hash_table_t *hash;
 	size_t i;
 
-	ht = malloc(sizeof(hash_table_t));
-
-	if (ht == NULL)
+	hash = malloc(sizeof(hash_table_t));
+	if (hash == NULL)
 	{
 		return (NULL);
-
 	}
-
-	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t *) * size);
-
-	if (ht->array == NULL)
+	hash->size = size;
+	hash->array = malloc(sizeof(hash_node_t *) * size);
+	if (!hash->array)
 	{
-		free(ht);
+		free(hash);
 		return (NULL);
-
 	}
-
 	for (i = 0; i < size; i++)
 	{
-		ht->array[i] = NULL;
+		hash->array[i] = NULL;
 	}
-
-	return (ht);
+	return (hash);
 }
