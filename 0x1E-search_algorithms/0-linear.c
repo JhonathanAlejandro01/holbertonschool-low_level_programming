a6c66276599a5fc7850544da41eaf0af30bb7540
JhonathanAlejandro01 <joaleanco@hotmail.com>
2020-08-27 11:23:07 -0500

    task 0 and .h

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,30 @@
+#include "search_algos.h"
+/**
+ * linear_search - Write a function that searches for a value in an array.
+ *@array: array to search.
+ *@size: size of array.
+ *@value: value search.
+ * Return: the value i.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%i] = [%i]\n", i, array[i]);
+		if (value == array[i])
+		{
+			return (i);
+		}
+		if ((i + 1) == size)
+		{
+			return (-1);
+		}
+	}
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,12 @@
+#ifndef _SEARCH_ALGOS_H_
+#define _SEARCH_ALGOS_H_
+/* for not double inclusion */
+/* the libraries standard */
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/* the prototype functions */
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
